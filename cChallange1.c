494367a7acd19f44f680424820c50e02fccf5b7e
NickyNocerino <nocerint@miamioh.edu>
2016-12-13 16:21:33 -0500

    second commit
    getting the hang of this

---
+++ cChallange1.c
@@ -0,0 +1,8 @@
+int main()
+{
+	printf("12345678901234567\n");
+	printf("Nicholas Nocerino\n");
+	printf("\"Nicholas\" is 8 letters long\n");
+	printf("\"Nocerino\" is 8 letters long\n");
+	return 0;
+}
